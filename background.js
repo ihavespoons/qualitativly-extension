879081f9a64eaf0d828efc7257690d7c71d97321
mwinmill@idea11.com.au <mwinmill@idea11.com.au>
2020-04-11 11:21:24 +1000

    updated

--- background.js
+++ background.js
@@ -1,28 +1,44 @@
 
-
-
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
-    
-    
-        chrome.pageCapture.saveAsMHTML({tabId: tabId}, function(mhtml){
             chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
                 let url = tabs[0].url;
                sendURL(url);
                 
-            });
+           
               });
-              
-            
-  
 
 })
 
+let value; 
+function userSetting(){
+   chrome.storage.sync.get(['fakeness'], function(result){
+        
+        value = result.fakeness;
+    })
+    
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log(document.getElementsByTagName("h3"));
+})
+
+
+function eval(fakenessScore,trustworthy){
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log(document.getElementById("trust"));
+        
+    console.log(x);
+})
+}
+
+
+
 function sendURL(url){
     const data = { article_url: url };
+    userSetting();
 
-    fetch('https://example.com/profile', {
+    fetch('https://api.qualitativly.com/classify/covid19', {
          method: 'POST', 
         headers: {
             'Content-Type': 'application/json',
@@ -31,7 +47,8 @@ function sendURL(url){
     })
     .then((response) => response.json())
     .then((data) => {
-    console.log('Success:', data);
+    
+    eval(data.untrustworthy, data.trustworth);
     })
     .catch((error) => {
     console.error('Error:', error);
