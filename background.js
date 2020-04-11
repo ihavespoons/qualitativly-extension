a57776958b8717ae88fcfee89ca4d7ac252d25cc
Lachlan <lach1230@hotmail.com>
2020-04-11 12:12:25 +1000

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit dad7d3c53a7318b2d60c04619b85cb7ebcbbe8b2, reversing
    changes made to 562e7bf517c49fae891cb3ddb6b005878bda1abc.

--- background.js
+++ background.js
@@ -1,44 +1,28 @@
 
+
+
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
+    
+    
+        chrome.pageCapture.saveAsMHTML({tabId: tabId}, function(mhtml){
             chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
                 let url = tabs[0].url;
                sendURL(url);
                 
-           
+            });
               });
+              
+            
+  
 
 })
-
-let value; 
-function userSetting(){
-   chrome.storage.sync.get(['fakeness'], function(result){
-        
-        value = result.fakeness;
-    })
-    
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    console.log(document.getElementsByTagName("h3"));
-})
-
-
-function eval(fakenessScore,trustworthy){
-    document.addEventListener('DOMContentLoaded', function () {
-        console.log(document.getElementById("trust"));
-        
-    console.log(x);
-})
-}
-
-
 
 function sendURL(url){
     const data = { article_url: url };
-    userSetting();
 
-    fetch('https://api.qualitativly.com/classify/covid19', {
+    fetch('https://example.com/profile', {
          method: 'POST', 
         headers: {
             'Content-Type': 'application/json',
@@ -47,8 +31,7 @@ function sendURL(url){
     })
     .then((response) => response.json())
     .then((data) => {
-    
-    eval(data.untrustworthy, data.trustworth);
+    console.log('Success:', data);
     })
     .catch((error) => {
     console.error('Error:', error);
