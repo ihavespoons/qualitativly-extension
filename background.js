aec3eba7d2dddf2ec013f7cdfe6fe6512ef551ee
mwinmill@idea11.com.au <mwinmill@idea11.com.au>
2020-04-11 12:19:14 +1000

    updated

--- background.js
+++ background.js
@@ -1,28 +1,58 @@
 
-
-
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
-    
-    
-        chrome.pageCapture.saveAsMHTML({tabId: tabId}, function(mhtml){
             chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
                 let url = tabs[0].url;
                sendURL(url);
                 
-            });
+           
               });
-              
-            
-  
 
 })
 
+let value; 
+function userSetting(){
+   chrome.storage.sync.get(['fakeness'], function(result){
+        
+        value = result.fakeness;
+    })
+    
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log(document.getElementsByTagName("h3"));
+})
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+        chrome.browserAction.setPopup({
+            tabId: tabId,
+            popup: 'index.html'
+        });
+
+    chrome.browserAction.getPopup({ tabId: tabId},function(result) {
+        window.alert(result);
+
+        console.log(document.getElementById("trust"));
+    })
+});
+
+
+function eval(fakenessScore,trustworthy){
+
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log(document.getElementById("trust"));
+        
+    console.log(x);
+})
+}
+
+
+
 function sendURL(url){
     const data = { article_url: url };
+    userSetting();
 
-    fetch('https://example.com/profile', {
+    fetch('https://api.qualitativly.com/classify/covid19', {
          method: 'POST', 
         headers: {
             'Content-Type': 'application/json',
@@ -31,7 +61,8 @@ function sendURL(url){
     })
     .then((response) => response.json())
     .then((data) => {
-    console.log('Success:', data);
+    
+    eval(data.untrustworthy, data.trustworth);
     })
     .catch((error) => {
     console.error('Error:', error);
--- popup.js
+++ popup.js
@@ -1,10 +1,54 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('conservative').addEventListener('click', main);      
+    document.getElementById('conservative').addEventListener('click', conservative)
+    document.getElementById('low').addEventListener('click', low);
+    document.getElementById('high').addEventListener('click', high);
+    document.getElementById('off').addEventListener('click', off);
+
+
+})
+
+function conservative(){
+    chrome.storage.sync.set({fakeness: "conservative"}, function() {
+        console.log('Value is set to ' + value);
+    })
+}
+
+
+function conservative(){
+    chrome.storage.sync.set({fakeness: 75}, function() {
+        console.log('Value is set to ' + "conservative");
+    })
+}
+function low(){
+    chrome.storage.sync.set({fakeness: 50}, function() {
+        console.log('Value is set to ' + "low");
+    })
+}
+function high(){
+    chrome.storage.sync.set({fakeness: 90}, function() {
+        console.log('Value is set to ' + "high");
+    })
+}
+function off(){
+    chrome.storage.sync.set({fakeness: "off"}, function() {
+        console.log('Value is set to ' + "off");
+    })
+}
+
+/*
+chrome.tabs.executeScript(null, { file: "jquery-3.4.1.js" }, function() {
+    chrome.tabs.executeScript(null, { file: "popups.js" });
 });
 
-function main() {
-    var mydata = JSON.stringify(userSetting);
-  console.log(mydata);
-}
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null); 
+}*/
+
